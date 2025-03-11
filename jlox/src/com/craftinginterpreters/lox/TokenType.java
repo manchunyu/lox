885d206901c470cbf0e5dcfcfc9bde85624aa182
Man-Chun Yu <154927141+manchunyu@users.noreply.github.com>
2025-03-11 00:41:50 +0000

    Add Token class
    with type, line, literal and lexeme data

--- jlox/src/com/craftinginterpreters/lox/TokenType.java
+++ jlox/src/com/craftinginterpreters/lox/TokenType.java
@@ -6,5 +6,17 @@ enum TokenType {
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
     // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
 
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
