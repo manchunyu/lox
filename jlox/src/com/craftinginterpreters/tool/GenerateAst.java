3398f298b4f5a3cbc2531de07e682659615cb939
Man-Chun Yu <154927141+manchunyu@users.noreply.github.com>
2025-03-19 22:33:37 +0000

    Create new file to automate writing expr class

---
+++ jlox/src/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+
+public class GenerateAst {
+     public static void main(String[] args) throws IOException {
+         if (args.length != 1) {
+             System.err.println("Usage: generate_ast <output directory>");
+             System.exit(64);
+         }
+         String outputDir = args[0];
+
+         
+     }
+}
